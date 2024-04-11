4a5198aee48a6a2af1e4e4d14ea343bb0251ef2d
tossapong nuntanart <tosssapong.n@appman.co.th>
2024-04-11 15:12:46 +0700

    initial

---
+++ index.js
@@ -0,0 +1,68 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+async function run() {
+  const browser = await puppeteer.launch({headless:false});
+  const page = await browser.newPage();
+  await page.goto('http://deka.supremecourt.or.th/search/year');
+  await page.screenshot({path:'example.png',fullPage:true})
+
+
+  // Step 2: Wait for the specific element to be available
+  // Use XPath to find an element with the specific class and text "2563"
+  const specificElementXPath = `xpath///span[@class='word-a-href' and text()='2566']`;
+  const linkEx = await page.waitForSelector(specificElementXPath);
+  await linkEx.click();
+ 
+
+  // Step 4: Wait for the navigation to complete
+  await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+
+ 
+  // Initialization of the CSV content with headers
+  let csvContent = "topic,doc_id\n";
+
+  let hasNextPage = true;
+
+  while (hasNextPage) {
+    // Wait for the elements to be loaded
+    await page.waitForSelector('.clear > ul', { timeout: 5000 });
+
+    // Retrieve and iterate through the list of elements
+    const listItems = await page.$$('.clear > ul');
+    for (const item of listItems) {
+      // Fetch the value from .deka-result and the text from label.css-label
+      const value = await item.$eval('.deka-result', el => el.value); // Make sure .deka-result is correct
+      const text = await item.$eval('label.css-label', el => el.textContent);
+
+      // Append the fetched data to the CSV string
+      csvContent += `"${text}","${value}"\n`;
+    }
+
+    // Check for the "Next Page" button and click if available
+    const nextPageButtonSelector = 'a:has(> span.glyphicon.glyphicon-chevron-right):not(:has(> span:nth-of-type(2)))';
+    const nextPageButton = await page.$(nextPageButtonSelector);
+    if (nextPageButton) {
+      await nextPageButton.click();
+      await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    } else {
+      hasNextPage = false; // Stop the loop if "Next Page" button is not found
+    }
+  }
+
+  // Step 5: Take a screenshot of the new page
+  await page.screenshot({ path: 'new_page_screenshot.png' });
+
+  // Write the CSV string to a file
+  fs.writeFileSync('data.csv', csvContent);
+
+
+
+  // Step 5: Take a screenshot of the new page
+  await page.screenshot({ path: 'new_page_screenshot.png' });
+
+  await browser.close();
+}
+
+run();
