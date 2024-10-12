54eedbfd6807f9c8078d93de12cc91c6c7f676aa
tossapong nuntanart <tosssapong.n@appman.co.th>
2024-10-12 23:16:44 +0700

    add vibe project

--- projectList.js
+++ projectList.js
@@ -70,7 +70,12 @@ const projectList = [
     {
       url: 'https://propertyhub.in.th/%E0%B9%80%E0%B8%8A%E0%B9%88%E0%B8%B2%E0%B8%84%E0%B8%AD%E0%B8%99%E0%B9%82%E0%B8%94/%E0%B9%82%E0%B8%84%E0%B8%A3%E0%B8%87%E0%B8%81%E0%B8%B2%E0%B8%A3-condolette-ize-ratchathewi',
       name: 'condolette-ize-ratchathewi'
+    },
+    {
+      url: 'https://propertyhub.in.th/%E0%B9%80%E0%B8%8A%E0%B9%88%E0%B8%B2%E0%B8%84%E0%B8%AD%E0%B8%99%E0%B9%82%E0%B8%94/%E0%B9%82%E0%B8%84%E0%B8%A3%E0%B8%87%E0%B8%81%E0%B8%B2%E0%B8%A3-the-line-vibe-phahonyothin',
+      name: 'line-vibe-phahonyothin'
     }
+    
   ];
 
   module.exports = { projectList };
